62219527fe81f5d3cc91c7f98902e8a46c078c21
mmayxx <mmayxx07@gmail.com>
2022-03-21 12:47:46 +0530

    routes @ -> idx

---
+++ Particulars/Data.js
@@ -0,0 +1,32 @@
+module.exports.Data = [
+    {
+        id_: 1,
+        company: 'Apple',
+        sym: 'AAPL',
+        eps: 1,
+    },
+    {
+        id_: 2,
+        company: 'Amazon',
+        sym: 'AMZ',
+        eps: 2,
+    },
+    {
+        id_: 3,
+        company: 'Microsoft',
+        sym: 'MSF',
+        eps: 3,
+    },
+    {
+        id_: 4,
+        company: 'Netflix',
+        sym: 'NFX',
+        eps: 4,
+    },
+    {
+        id_: 5,
+        company: 'Airbnb',
+        sym: 'ARB',
+        eps: 5,
+    }
+]
--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
+const $_data = require('./Particulars/Data')
 
 app.set('views', (__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.get('/', function(req, res, next) {
-    res.render('index')
+    res.render('index', {Data_: $_data['Data']})
 })
-app.listen(port, () => {console.log(`super-giggle listening on port : ${port}`)})
+app.get('/:id', function(req, res, next) {
+    $_params = req.params.id
+    res.render('resp', { id_ : $_params})
+})
+app.listen(port, () => {console.log(`super-giggle running on port : ${port}`)})
